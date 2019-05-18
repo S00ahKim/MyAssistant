b104b8747d7f43a83558eb0e5fc13db80c4ffeba
S00ahKim <rlatndk0206@naver.com>
2019-05-18 21:45:30 +0900

    카드뷰 추가

--- app/src/main/java/com/cookandroid/myassistant/Market.java
+++ app/src/main/java/com/cookandroid/myassistant/Market.java
@@ -2,9 +2,18 @@ package com.cookandroid.myassistant;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.TabHost;
 
+import java.util.ArrayList;
+
 public class Market extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private ArrayList<MyData> myDataset;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +43,27 @@ public class Market extends AppCompatActivity {
         ts4.setContent(R.id.content4);
         ts4.setIndicator("기타");
         tabHost1.addTab(ts4);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        myDataset = new ArrayList<>();
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
+        // 이 부분으로 데이터를 추가하는 것!!
+        myDataset.add(new MyData("루시아", "핑크조아", "#분홍머리, #공주님", R.drawable.favitem1));
+        myDataset.add(new MyData("루시아", "댕댕이", "#멍멍이와_함께, #안경, #발랄", R.drawable.favitem2));
+        myDataset.add(new MyData("루시아", "MyAssistant", "#기본이_최고", R.drawable.defaultimage));
+
 
     }
 }
