01661d347cbd94858a6db1d1895078b89da10fd4
S00ahKim <rlatndk0206@naver.com>
2019-06-01 09:30:04 +0900

    디자인 수정

--- app/src/main/java/com/cookandroid/myassistant/AddNewStyleSkin.java
+++ app/src/main/java/com/cookandroid/myassistant/AddNewStyleSkin.java
@@ -1,8 +1,14 @@
 package com.cookandroid.myassistant;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -22,13 +28,14 @@ public class AddNewStyleSkin extends AppCompatActivity {
         mws.setLoadWithOverviewMode(true);//컨텐츠가 웹뷰보다 클 경우 스크린 크기에 맞게 조정
 
         mwv.setWebViewClient(new WebViewClient(){
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                final Uri uri = request.getUrl();
                 return true;
             }
         });
-        mwv.loadUrl("https://picrew.me/image_maker/41329");
+        mwv.loadUrl("https://kr.vonvon.me/quiz/604");
     }
 
     //   추가전에 뒤로가기 이벤트 호출시 홈으로 돌아갔으나, 이젠 일반적인 뒤로가기 기능 활성화
--- app/src/main/java/com/cookandroid/myassistant/EditAssistantACC.java
+++ app/src/main/java/com/cookandroid/myassistant/EditAssistantACC.java
@@ -1,8 +1,11 @@
 package com.cookandroid.myassistant;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,6 +15,14 @@ public class EditAssistantACC extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_assistant_acc);
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+//
+//        //툴바 설정
+//        toolbar.setTitleTextColor(Color.parseColor("#84C1FF")); //제목의 칼라
+//        toolbar.setTitle("");
+//        setSupportActionBar(toolbar); //툴바를 액션바와 같게 만들어 준다.
+//        getSupportActionBar().setTitle("Acc");
 
         Button prevButton = (Button) findViewById(R.id.prev) ;
         Button nextButton = (Button) findViewById(R.id.next) ;
